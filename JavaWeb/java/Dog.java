7f5e8e61d8b28172db5db6fa0fc9b9118d87a488
amicodea <everrobin@gmail.com>
2024-07-12 17:50:28 +0900

    7-12

---
+++ JavaWeb/java/Dog.java
@@ -0,0 +1,27 @@
+
+public class Dog {
+	public static void main(String[] args) {
+		System.out.println("Dog");
+	}
+	
+	String breed;
+	int size;
+	String colour;
+	int age;
+	
+	void eat() {
+		
+	}
+	
+	void run() {
+		
+	}
+	
+	void sleep() {
+		
+	}
+	
+	void name() {
+		
+	}
+}
